a1f39296a53c6c39aacc21f6f9657abe39483e72
NaitikSharma-09 <vs19671702@gmail.com>
2026-05-05 19:12:35 +0530

    project structure ready

--- Naitik.py
+++
@@ -1,9 +0,0 @@
-import random
-
-print("Hello bitches! ")
-print("Enter Your name:")
-name = input()
-print("Hello ",name)
-random_letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-chosen_letter = random.choice(random_letter)
-print("Your random letter is:", chosen_letter)
